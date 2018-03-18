0dce205c4da3585a50478d11316672cab57a54c8
saigo <saigo@localhost.localdomain>
2018-03-18 17:38:58 +0900

    user and restaurant models has many through pin created

---
+++ app/models/restaurant.rb
@@ -0,0 +1,4 @@
+class Restaurant < ApplicationRecord
+  has_many :pins
+  has_many :users, through: :pins
+end
---
+++ db/migrate/20180318082129_create_pins.rb
@@ -0,0 +1,10 @@
+class CreatePins < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pins do |t|
+      t.integer :restaurant_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
