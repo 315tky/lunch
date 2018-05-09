4d4c17a59d4e3faeec898114ede98fcddcc8e194
315tky <315tky@gmail.com>
2018-05-09 07:58:45 +0000

    action mailer configured

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,22 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'lunch-dev.saigos.space'
+  config.action_mailer.default_url_options = { host: host }
+
+  config.action_mailer.smtp_settings = {
+    address:        'smtp.gmail.com',
+    port:           587,
+    domain:         'lunch-dev.saigos.space',
+    user_name:      'lunchapp.notifications@gmail.com',
+    password:       'In0de!lunchapp',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
