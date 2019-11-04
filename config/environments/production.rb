de1f0284a833d21023d8a854df9611b349d5bcd2
315tky <315tky@gmail.com>
2019-11-04 23:22:40 +0000

    fixed mailer and a few other odd old changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:        'smtp.gmail.com',
     port:           587,
-    domain:         'lunch-dev.saigos.space',
+    domain:         'saigos.space',
+    #domain:         'gmail.com',
     user_name:      Figaro.env.gmail_username, 
     password:       Figaro.env.gmail_password,
     authentication: 'plain',
